fc2922d467db7d96be42e5a8c9a4fdd3a03a1ad0
sakshimatiray <113632675+sakshimatiray@users.noreply.github.com>
2022-11-27 11:13:47 +0530

    Add files via upload

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+// write code for Welcome component here
+import React from "react";
+const Welcome = (props)=>{
+    return(
+    <div><h1>{props.name}!</h1>
+    <h1>Welcome to Newton School</h1></div>
+    )
+}
+export default Welcome
