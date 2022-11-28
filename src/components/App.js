9cb9f80c9b9d44da9cc37f180ee06aa7400b6df5
sakshi <sakshimatiray96628@gmail.com>
2022-11-28 16:03:59 +0530

    tik

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react'
 import '../styles/App.css';
-import Welcome from './Welcome';
-
+const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
+const Join = (props) =>{
+  return(
+    <div id ="join">
+      {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      <p>{props.words.join(",")}</p>
+    </div>
+  );
+};
 const App = () => {
-  return (
 
-    <h2>Welcome to Newton School.</h2>
-  )
-}
+  return (
+    <div id="main">
+      <Join words={arr} />
+    </div>
+  );
+};
 
 
 export default App;
